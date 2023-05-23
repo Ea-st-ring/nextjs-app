81f82f20772308b34e20dd682065d5f338305fc6
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-05-24 00:43:07 +0900

    [chore] 오타 수정

--- lib/posts.ts
+++ lib/posts.ts
@@ -13,18 +13,17 @@ const postsDirectory = path.join(process.cwd(), 'posts');
 export function getSortedPostsData(){
     // /posts 파일 이름을 잡아주기
     const fileNames = fs.readdirSync(postsDirectory)
-
+    
     const allPostsData = fileNames.map(fileName => {
         const id = fileName.replace(/\.md$/,"");
 
         const fullPath = path.join(postsDirectory, fileName);
         const fileContents = fs.readFileSync(fullPath, 'utf-8')
-
         const matterResult = matter(fileContents);
 
         return {
             id,
-            ...matterResult.data as {data: string; title: string}
+            ...matterResult.data as {date: string; title: string},
         }
     })
 
@@ -36,6 +35,7 @@ export function getSortedPostsData(){
         else return -1
     })
 }
+
 
 export function getAllPostIds(){
     const fileNames = fs.readdirSync(postsDirectory);
